df4bd520a950352ad24eb269ab23599c54dbd33a
Brayan Santos <bdsantosa@udistrital.edu.co>
2026-08-19 17:21:08 -0500

    feat: Add observer arraylists

--- src/data/WeatherData.java
+++ src/data/WeatherData.java
@@ -1,39 +1,53 @@
 package data;
 import subject.Subject;
+import observers.Observer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeatherData implements Subject{
 	
+	private List<Observer> observers = new ArrayList<Observer>();
+	private float temperature;
+	private float humidity;
+	private float pressure;
 
-	public void registerObserver() {
+	public void registerObserver(Observer o) {
 		System.out.println("Suscribete para recibir notificaciones");
-		
+		observers.add(o);
 	}
 
-	public void removeObserver() {
+	public void removeObserver(Observer o) {
 		System.out.println("Desuscribete si ya no quieres recibir información");
-		
+		observers.remove(o);
 	}
 
 	public void notifyObservers() {
 		System.out.println("Hay una nueva notificación del clima");
-		
+		for (Observer observer : observers) {
+			observer.update(temperature, humidity, pressure);
+		}
 	}
 	
 	public float getTemperature() {
-		return 0;
+		return temperature;
 	}
 	
 	public float getHumidity() {
-		return 0;
+		return humidity;
 	}
 	public float getPressure() {
-		return 0;
+		return pressure;
 	}
 	
 	public void meassurementsChanged(){
-		float temp= getTemperature();
-		float humidity = getHumidity();
-		float pressure = getPressure();
+		notifyObservers();
+	}
+	
+	public void setMeasurements(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		this.pressure = pressure;
+		meassurementsChanged();
 	}
 	
 	

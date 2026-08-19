55608d5fa7ae37c75931a16f9ab8fa132902d574
Brayan Santos <bdsantosa@udistrital.edu.co>
2026-08-19 17:26:34 -0500

    feat: Add update stadistics with max temperature and min temperature

--- src/observers/StadisticsDisplay.java
+++ src/observers/StadisticsDisplay.java
@@ -1,12 +1,35 @@
 package observers;
+import data.WeatherData;
 
-public class StadisticsDisplay implements Observer{
+public class StadisticsDisplay implements Observer, DisplayElement{
 
-	public void update() {
-		
+	private float maxTemp = Float.MIN_VALUE;
+	private float minTemp = Float.MAX_VALUE;
+	private float tempSum = 0.0f;
+	private int numReadings = 0;
+	private WeatherData weatherData;
+
+	public StadisticsDisplay(WeatherData weatherData) {
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+
+	public void update(float temperature, float humidity, float pressure) {
+		tempSum += temperature;
+		numReadings++;
+
+		if (temperature > maxTemp) {
+			maxTemp = temperature;
+		}
+		if (temperature < minTemp) {
+			minTemp = temperature;
+		}
+		Display();
 	}
 	
-	public void display() {
+	public void Display() {
 		//Stadistics Display
+		System.out.println("Estadisticas: promedio/max/min = " + (tempSum / numReadings) + "/" + maxTemp + "/" + minTemp);
 	}
 }
+
